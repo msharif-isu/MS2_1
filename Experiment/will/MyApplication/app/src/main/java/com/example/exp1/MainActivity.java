2b6d07d8a6b435ce8d757897d659e97bad131893
Will Peyer <wpeyer@iastate.edu>
2024-02-02 15:03:08 -0600

    experiment 1

---
+++ Experiment/will/MyApplication/app/src/main/java/com/example/exp1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        textView.setText("Hello World!");
+    }
+
+    public void updateText(View view) {
+        textView.setText("Goodbye!");
+        System.out.println("The button was clicked.");
+    }
+}

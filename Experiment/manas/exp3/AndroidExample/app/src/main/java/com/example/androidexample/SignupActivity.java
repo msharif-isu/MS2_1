b7b7571b9a008f6943d3968f7699afb18f0c45d5
Manas Mathur <manas1@iastate.edu>
2024-02-02 13:21:16 -0600

    Experiment 3

---
+++ Experiment/manas/exp3/AndroidExample/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -0,0 +1,91 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;  // define username edittext variable
+    private EditText passwordEditText;  // define password edittext variable
+    private EditText confirmEditText;   // define confirm edittext variable
+    private Button loginButton;         // define login button variable
+    private Button signupButton;        // define signup button variable
+
+    private Button randomButton; // define randomize password variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        /* initialize UI elements */
+        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
+        passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
+        confirmEditText = findViewById(R.id.signup_confirm_edt);    // link to confirm edtext in the Signup activity XML
+        loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
+        signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
+
+        randomButton  = findViewById(R.id.signup_randomize_btn); // link to randomize password button in the signup activity
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);  // go to LoginActivity
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* grab strings from user inputs */
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm = confirmEditText.getText().toString();
+
+                if (password.equals(confirm)){
+                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                Random rand = new Random();
+
+                int passLen = 8 + rand.nextInt(3);
+
+                StringBuilder builder = new StringBuilder(passLen);
+                for (int i = 0; i < passLen; i++) {
+                    int ind = rand.nextInt(characters.length());
+                    builder.append(characters.charAt(ind));
+                }
+
+                String password = builder.toString();
+                passwordEditText.setText(password);
+                confirmEditText.setText(password);
+
+                Toast.makeText(getApplicationContext(), "Password is: " + password, Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}

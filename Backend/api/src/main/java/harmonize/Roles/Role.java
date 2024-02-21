bb35dc4e696824e298595c442fa3cd5cd7a74431
phun <phun5701@gmail.com>
2024-02-21 00:11:25 -0600

    Added role entity and role repository

---
+++ Backend/api/src/main/java/harmonize/Roles/Role.java
@@ -0,0 +1,23 @@
+package harmonize.Roles;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "roles")
+@Entity
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}   
---
+++ Backend/api/src/main/java/harmonize/Roles/RoleRepository.java
@@ -0,0 +1,9 @@
+package harmonize.Roles;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}

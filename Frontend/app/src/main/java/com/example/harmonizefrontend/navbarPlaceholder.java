672266598518402e5e122b7bb4034f437097d0ac
Manas Mathur <manas1@iastate.edu>
2024-02-28 14:26:10 -0600

    test commit

---
+++ Frontend/app/src/main/java/com/example/harmonizefrontend/navbarPlaceholder.java
@@ -0,0 +1,14 @@
+package com.example.harmonizefrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class navbarPlaceholder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navbar_placeholder);
+    }
+}

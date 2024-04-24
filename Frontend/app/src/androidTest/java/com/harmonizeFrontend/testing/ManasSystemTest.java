1f7af90a7ddad04a83d38bbd4dd39b52d784910f
Manas Mathur <manas1@iastate.edu>
2024-04-24 16:54:35 -0500

    Wrote a couple tests, for some reason I cant run coverage reports?

--- Frontend/app/src/androidTest/java/com/harmonizeFrontend/testing/ManasSystemTest.java
+++ Frontend/app/src/androidTest/java/com/harmonizeFrontend/testing/ManasSystemTest.java
@@ -1,13 +1,17 @@
 package com.harmonizeFrontend.testing;
 
 import com.example.harmonizefrontend.*;
-//import com.example.harmonizefrontend.navBar;
-//import com.example.harmonizefrontend.LoginScreen;
-//import com.example.harmonizefrontend.RegistrationScreen;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.Root;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
@@ -26,6 +30,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
 import android.view.View;
 
+
 /**
  * This testing file uses ActivityScenarioRule instead of ActivityTestRule
  * to demonstrate system testings cases
@@ -36,52 +41,129 @@ public class ManasSystemTest {
 
     private static final int SIMULATED_DELAY_MS = 500;
 
-    @Rule
-    public ActivityScenarioRule<navBar> activityScenarioRuleMain = new ActivityScenarioRule<>(navBar.class);
+//    @Rule
+//    public ActivityScenarioRule<navBar> activityScenarioRuleMain = new ActivityScenarioRule<>(navBar.class);
 
     @Rule
     public ActivityScenarioRule<LoginScreen> activityScenarioRuleLogin = new ActivityScenarioRule<>(LoginScreen.class);
 
-    @Rule
-    public ActivityScenarioRule<RegistrationScreen> activityScenarioRuleRegistration = new ActivityScenarioRule<>(RegistrationScreen.class);
+//    @Rule
+//    public ActivityScenarioRule<RegistrationScreen> activityScenarioRuleRegistration = new ActivityScenarioRule<>(RegistrationScreen.class);
+
+//    /*
+//     * Test Case 1: User logs in with invalid re-entered password. Test should check for if a toast posts up and tells the user that the second password is incorrect.
+//     * User then attempts to re-enter the second password and logs in successfully.
+//     */
+//    @Test
+//    public void inputInvalidRegistrationDetails() {
+//        String first = "Doug";
+//        String last = "Dimmadome";
+//        String username = "DougDimmadome45";
+//        String password = "DimmaDoug45!";
+//        String wrongPass = "Doug2";
+//        activityScenarioRuleRegistration.getScenario().onActivity(activity -> {
+////           activity.username = "Null";
+////           activity.first = "Doug";
+////           activity.last = "Dimmadome";
+////           activity.password = "pass";
+////           activity.checkPass = "pass";
+//        });
+//
+//        // Writes in first name into the first name box
+//        onView(withId(R.id.firstName)).perform(typeText(first), closeSoftKeyboard());
+//        // Writes in last name into the last name box
+//        onView(withId(R.id.lastName)).perform(typeText(last), closeSoftKeyboard());
+//        // Writes in the username into the last name box
+//        onView(withId(R.id.UsernameInput)).perform(typeText(username), closeSoftKeyboard());
+//        // Writes in the password into the password box
+//        onView(withId(R.id.PasswordInput)).perform(typeText(password), closeSoftKeyboard());
+//        // Writes in the double-check password into the re-enter password box
+//        onView(withId(R.id.reenterPassword)).perform(typeText(wrongPass), closeSoftKeyboard());
+//
+//        // Clicks the register button
+//        onView(withId(R.id.Register)).perform(click());
+//
+//        // Check to see if the toast message with "Please ensure that the passwords match" is displayed}
+//        //TODO FINISH THIS
+//
+//    }
 
     /*
-     * Test Case 1: User logs in with invalid re-entered password. Test should check for if a toast posts up and tells the user that the second password is incorrect.
-     * User then attempts to re-enter the second password and logs in successfully.
+    * Logs a user in and verifies that credentials are correct in the AccountPreferences fragment
      */
+
     @Test
-    public void inputInvalidRegistrationDetails() {
-        String first = "Doug";
-        String last = "Dimmadome";
-        String username = "DougDimmadome45";
-        String password = "DimmaDoug45!";
-        String wrongPass = "Doug2";
-        activityScenarioRuleRegistration.getScenario().onActivity(activity -> {
-//           activity.username = "Null";
-//           activity.first = "Doug";
-//           activity.last = "Dimmadome";
-//           activity.password = "pass";
-//           activity.checkPass = "pass";
+    public void checkUserDetailsAfterLogin() {
+        // Uses activityScenarioRuleLogin
+        String username = "Manasmathur2023";
+        String password = "Backup890!";
+        activityScenarioRuleLogin.getScenario().onActivity(activity -> {
+
         });
 
-        // Writes in first name into the first name box
-        onView(withId(R.id.firstName)).perform(typeText(first), closeSoftKeyboard());
-        // Writes in last name into the last name box
-        onView(withId(R.id.lastName)).perform(typeText(last), closeSoftKeyboard());
-        // Writes in the username into the last name box
+        // Writes in the username into the username box
         onView(withId(R.id.UsernameInput)).perform(typeText(username), closeSoftKeyboard());
+
         // Writes in the password into the password box
         onView(withId(R.id.PasswordInput)).perform(typeText(password), closeSoftKeyboard());
-        // Writes in the double-check password into the re-enter password box
-        onView(withId(R.id.reenterPassword)).perform(typeText(wrongPass), closeSoftKeyboard());
 
-        // Clicks the register button
-        onView(withId(R.id.Register)).perform(click());
+        // Clicks the login button
+        onView(withId(R.id.Login)).perform(click());
 
-        // Check to see if the toast message with "Please ensure that the passwords match" is displayed
-        onView(withText("Please ensure that the passwords match"))
-                .inRoot(withDecorView(ViewMatchers.is(activityScenarioRuleRegistration.getActivity().getWindow().getDecorView())))
-                .check(matches(isDisplayed()));
+        // Check to see if its taken you to the navbar screen and the profile Preferences fragment
+        ViewInteraction dashboard = onView(withText("This is profile preferences fragment")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        // Check to see if R.id.usernameHolder matches username variable
+        onView(withId(R.id.usernameHolder)).check(ViewAssertions.matches(withText(username)));
+
+        // Check to see if password matches username variable
+        onView(withId(R.id.hideunhide)).perform(click());
+        onView(withId(R.id.password_text_view)).check(ViewAssertions.matches(withText(password)));
+        onView(withId(R.id.hideunhide)).perform(click());
+    }
+
+
+    /*
+    * This test checks the ability for the user to change their first and last name
+     */
+    @Test
+    public void changeFirstLastName() {
+        String username = "Manasmathur2023";
+        String password = "Backup890!";
+        activityScenarioRuleLogin.getScenario().onActivity(activity -> {
+
+        });
+
+        // Writes in the username into the username box
+        onView(withId(R.id.UsernameInput)).perform(typeText(username), closeSoftKeyboard());
+
+        // Writes in the password into the password box
+        onView(withId(R.id.PasswordInput)).perform(typeText(password), closeSoftKeyboard());
+
+        // Clicks the login button
+        onView(withId(R.id.Login)).perform(click());
+
+        // Check to see if its taken you to the navbar screen and the profile Preferences fragment
+        ViewInteraction dashboard = onView(withText("This is profile preferences fragment")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        // Turn on edit mode
+        onView(withId(R.id.updatePrefs)).perform(click());
+        // Edit mode on
+        onView(withId(R.id.firstName)).perform(typeText("Sanam"), closeSoftKeyboard());
+        onView(withId(R.id.lastName)).perform(typeText("Ruhtam"), closeSoftKeyboard());
+
+        onView(withId(R.id.updatePrefs)).perform(click());
+        // Edit mode off
+
+        // Wait 1500ms
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {}
+
+        // Check to see if first name and last name has saved
+        onView(withId(R.id.firstname)).check(ViewAssertions.matches(withText("Sanam")));
+        onView(withId(R.id.lastname)).check(ViewAssertions.matches(withText("Ruhtam")));
+
     }
 
 }

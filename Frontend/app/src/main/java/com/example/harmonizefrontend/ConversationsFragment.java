c0b075daf75c3deb31db0a8e48d7b48366bd37e3
Manas Mathur <manas1@iastate.edu>
2024-04-30 15:27:58 -0500

    Working on adding groupchats

--- Frontend/app/src/main/java/com/example/harmonizefrontend/ConversationsFragment.java
+++ Frontend/app/src/main/java/com/example/harmonizefrontend/ConversationsFragment.java
@@ -50,7 +50,7 @@ public class ConversationsFragment extends Fragment implements WebSocketListener
     private List<ConversationDTO> conversations;
     private Map<Integer, ConversationDTO> listConversations;
 
-    private ImageButton deleteConvos;
+    private ImageButton addGC;
 
     private RequestQueue mQueue = UserSession.getInstance().getmQueue();
 
@@ -121,30 +121,21 @@ public class ConversationsFragment extends Fragment implements WebSocketListener
         view = inflater.inflate(R.layout.fragment_conversation_list, container, false);
 
         recyclerView = view.findViewById(R.id.recycler);
-        deleteConvos = view.findViewById(R.id.deleteConvo);
+        addGC = view.findViewById(R.id.addGroupChat);
 //        view.findViewById(R.id.deleteConvo).setVisibility(View.GONE);
         conversations = getConversations();
         conversationListAdapter = new ConversationListAdapter(conversations, clickListener);
         recyclerView.setAdapter(conversationListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        deleteConvos.setOnClickListener(new View.OnClickListener() {
+        addGC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Conversation", String.valueOf(UserSession.getInstance().getSelectedConversations().size()));
-                ArrayList<ConversationDTO> tempConvos = UserSession.getInstance().getSelectedConversations();
-                for (ConversationDTO convo : tempConvos) {
-                    int convoId = convo.getDataId();
-                    deleteConversation(convoId, new VolleyCallBack() {
-                        @Override
-                        public void onSuccess() {
-                            Log.d("Conversation", "Conversation id: " + convoId + " deleted");
-                        }
-                    });
-                }
-                ((navBar) getActivity()).loadFragment(new ConversationsFragment());
+//                ((navBar) getActivity()).loadFragment(new addGroupsFragment);
             }
         });
+
+
 
 
         return view;
